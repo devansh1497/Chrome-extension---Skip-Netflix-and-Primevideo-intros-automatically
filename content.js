1cc0d8f8dd579e196666df6aa57d8a0507ec2f86
devansh <devanshsingh1997@gmail.com>
2020-07-14 13:27:27 +0530

    added prime support

--- content.js
+++ content.js
@@ -1,8 +1,15 @@
 
 const config = { attributes: true, childList: true, subtree: true };
-const callback = (list, obs) => {
+console.log('called')
+const callback = list => {
     for(const mut of list){
         if(mut.addedNodes && mut.addedNodes[0]){
+            //For prime
+            var ele = $(".f1oordr3.f989gul.f1ap0gh8.fq4bwzw.f1ns8ocy.fx1l1v6.f19qnh9o");
+            if(ele && ele[0]){
+                ele[0].click();
+            }
+            //For netflix
             var ele = $(".nf-icon-button.nf-flat-button.nf-flat-button-uppercase.no-icon");
             if(ele && ele[0]){
                 ele[0].click();
